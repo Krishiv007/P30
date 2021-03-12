ccf37593ccbe4b08d76c45aa7887404153653d59
Krishiv007 <76518814+Krishiv007@users.noreply.github.com>
2021-03-12 17:44:49 +0530

    Add files via upload

--- block.js
+++ block.js
@@ -1,48 +1,39 @@
+  
 class Block{
-    constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        this.Visiblity = 255;
-        World.add(world, this.body);
-      }
-      display(){
-
-        if(this.body.speed<3)
-        {
-          if(this.Visiblity===255)
-          {
-            push();
-            fill("cyan");
-            translate(this.body.position.x, this.body.position.y);
-            rectMode(CENTER);
-            rect(0, 0, this.width, this.height);
-            pop();
-           }
-          else
-          {
-            push();
-            fill("cyan");
-            this.Visiblity = this.Visiblity - 5;
-            tint(255,this.Visiblity);
-            rect(this.body.position.x, this.body.position.y, this.width, this.height);
-            pop();
-          }
-        }
-        else
-        {
-          World.remove(world, this.body);
-          push();
-          fill("cyan");
-          this.Visiblity = this.Visiblity - 5;
-          tint(255,this.Visiblity);
-          rect(this.body.position.x, this.body.position.y, this.width, this.height);
-          pop();
-         }
-       }
+  constructor(x, y, width, height) {
+    var options = {
+       
+        restitution :0.4,
+        friction :0.0,
+        //isStatic:true
+       
+    }
+    this.visibility = 225;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+    
+  }
+ 
+  display(){
+    console.log(this.body.speed);
+    if(this.body.speed <3){
+    var angle = this.body.angle;
+    var pos= this.body.position;
+    push();
+    fill("cyan");
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0,0,this.width, this.height);
+    pop();
+   }else{
+    World.remove(world, this.body);
+    push();
+    this.visibility = this.visibility -5;
+    pop();
+    
+   }
+ }
  }
